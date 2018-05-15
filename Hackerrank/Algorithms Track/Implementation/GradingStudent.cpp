7bbe09bc0323751a47c13a06a041f7f120625c27
sahilsingh21 <ss3842486@gmail.com>
2018-05-15 22:28:13 +0530

    solved gradingStudent problem

---
+++ Hackerrank/Algorithms Track/Implementation/GradingStudent.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+ * Complete the gradingStudents function below.
+ */
+vector<int> gradingStudents(vector<int> grades) {
+    /*
+     * Write your code here.
+     */
+    for(int i=0;i<grades.size();i++)
+    if(grades[i] >= 38){
+        int num;
+        int mul;
+        int sub;
+        num = (grades[i]/5);    
+        mul = 5*(num+1);
+        sub = (mul - grades[i]);
+        if(sub < 3){
+            grades[i] = mul;
+        }
+    }
+return grades;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    vector<int> grades(n);
+
+    for (int grades_itr = 0; grades_itr < n; grades_itr++) {
+        int grades_item;
+        cin >> grades_item;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        grades[grades_itr] = grades_item;
+    }
+
+    vector<int> result = gradingStudents(grades);
+
+    for (int result_itr = 0; result_itr < result.size(); result_itr++) {
+        fout << result[result_itr];
+
+        if (result_itr != result.size() - 1) {
+            fout << "\n";
+        }
+    }
+
+    fout << "\n";
+
+    fout.close();
+
+    return 0;
+}
