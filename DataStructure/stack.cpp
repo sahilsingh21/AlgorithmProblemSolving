500e3867b03852dbdfc6a92d08fe62b1f87c00af
sahilsingh21 <ss3842486@gmail.com>
2018-06-15 01:02:29 +0530

    In stack when we run display twise code will be dumbbed

--- DataStructure/stack.cpp
+++ DataStructure/stack.cpp
@@ -8,7 +8,6 @@ struct stack
 };
 struct stack* head = new struct stack;
 
-
 struct stack* AddNode(int val)
 {       
         head->val = val;
@@ -34,24 +33,56 @@ void Display(stack* pt)
 }
 
 int main()
-{
-        
-        struct stack* node = new struct stack;
-        head = node;
-        head->next = NULL;
-        AddNode(1);
-        AddNode(2);
-        AddNode(3);
-        AddNode(4);
-        AddNode(5);
-        AddNode(6);
-        AddNode(7);
-        AddNode(8);
-        struct stack* tail = head;
-        head = head->next;
-        tail->next = NULL;
-        Display(head);
-        Display(head);
-        Display(head);
+{      
+       cout<<"Enter your choise : ";
+       int n;
+       cin>>n;
+       cout<<endl;
+        while(n != 3){
+            if(n == 1){
+                int a;
+                cout<<"Enter the value : ";
+                cin>>a;
+                cout<<endl;
+                AddNode(a);
+                cout<<"Enter your choise : ";
+                cin>>n;
+                cout<<endl;
+            }else
+            
+            if(n == 2){
+                if(head->val == 0){
+                    struct stack* tail = head;
+                    head = head->next;
+                    tail->next = NULL;
+                }
+                Display(head);
+                cout<<"Enter your choise : ";
+                cin>>n;
+                cout<<endl;
+            }else
+            {
+                n = 3;
+            }
+                       
+        }
+
+        // struct stack* node = new struct stack;
+        // head = node;
+        // head->next = NULL;
+        // AddNode(1);
+        // AddNode(2);
+        // AddNode(3);
+        // AddNode(4);
+        // AddNode(5);
+        // AddNode(6);
+        // AddNode(7);
+        // AddNode(8);
+        // struct stack* tail = head;
+        // head = head->next;
+        // tail->next = NULL;
+        // Display(head);
+        // Display(head);
+        // Display(head);
     return 0;
 }
