02073d9bafe7823ed9f7392032d8095275f568d9
sahilsingh21 <ss3842486@gmail.com>
2021-05-26 15:00:43 +0530

    Revision of LinkList

---
+++ DataStructure/LinkList/hello.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+//#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+};
+
+void PrintList(Node *n)
+{
+    while (n != NULL)
+    {
+        cout << n->data << " ";
+        n = n->next;
+    }
+}
+
+int main()
+{
+    Node *head = NULL;
+    Node *second = NULL;
+    Node *third = NULL;
+
+    head = new Node;
+    second = new Node;
+    third = new Node;
+
+    head->data = 1;
+    head->next = second;
+
+    second->data = 2;
+    second->next = third;
+
+    third->data = 3;
+    third->next = NULL;
+
+    PrintList(head);
+
+    return 0;
+}
