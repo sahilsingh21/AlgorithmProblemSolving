92758e74c69157533fdb60c0b240ee4f073f925e
sahilsingh21 <ss3842486@gmail.com>
2021-06-04 13:08:19 +0530

    Finding dublicate string only in capital

--- DataStructure/String/dublicatString.cpp
+++ DataStructure/String/dublicatString.cpp
@@ -4,6 +4,27 @@ using namespace std;
 
 int main()
 {
-    string st = "geeksforgeeks";
+    int arr[27][2] = {0};
+    string st = "GEEKSFORGEEKS";
+
+    for (int i = 0; i < st.size(); i++)
+    {
+        int temp = (st[i] - 65);
+        // cout << temp << " ";
+        temp = temp % 27;
+        // cout << temp << endl;
+        arr[temp][1]++;
+    }
+
+    for (int i = 0; i < 26; i++)
+    {
+        if (arr[i][1] > 1)
+        {
+            char temp = 'A' + i;
+            char a = temp;
+            cout << a << ", count = " << arr[i][1] << endl;
+        }
+    }
+
     return 0;
 }
