22e16ce1c8c5fe42835da7d4bb9f4f71f639744a
sahilsingh21 <ss3842486@gmail.com>
2021-06-04 13:14:38 +0530

    Finding dublicate string for both Capital and small letters

--- DataStructure/String/dublicatString.cpp
+++ DataStructure/String/dublicatString.cpp
@@ -5,15 +5,22 @@ using namespace std;
 int main()
 {
     int arr[27][2] = {0};
-    string st = "GEEKSFORGEEKS";
+    string st = "GeeksForGeeks";
 
     for (int i = 0; i < st.size(); i++)
     {
-        int temp = (st[i] - 65);
-        // cout << temp << " ";
-        temp = temp % 27;
-        // cout << temp << endl;
-        arr[temp][1]++;
+        if (st[i] >= 65 && st[i] <= 90)
+        {
+            int temp = (st[i] - 65);
+            temp = temp % 27;
+            arr[temp][1]++;
+        }
+        else if (st[i] >= 97 && st[i] <= 122)
+        {
+            int temp = (st[i] - 97);
+            temp = temp % 27;
+            arr[temp][1]++;
+        }
     }
 
     for (int i = 0; i < 26; i++)
