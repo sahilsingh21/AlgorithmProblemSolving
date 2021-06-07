2ece84f4c2afd1ff6d549c30bfc9b890a8d0b923
sahilsingh21 <ss3842486@gmail.com>
2021-06-07 15:19:38 +0530

    bit manupalation learning

---
+++ DataStructure/Bit-Manipulation/basic.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int getBit(int n, int pos)
+{
+    return ((n & (1 << pos)) != 0);
+}
+
+int setBit(int n, int pos)
+{
+    return (n | (1 << pos));
+}
+
+int clearBit(int n, int pos)
+{
+    int mask = ~(1 << pos);
+    return (n & mask);
+}
+
+int updateBit(int n, int pos, int value)
+{
+    int mask = ~(1 << pos);
+    n = (n & mask);
+    return (n | (value << pos));
+}
+
+int main()
+{
+    cout << getBit(5, 1) << endl;
+
+    return 0;
+}
