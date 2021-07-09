17456fc399d0914d4d78dd2330170e1320ce3beb
sahilsingh21 <ss3842486@gmail.com>
2021-07-09 16:47:41 +0530

    sovling tower of hanoi problem with recursion

---
+++ DataStructure/Recursion/towerOfHenoi.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void towerOfHanoi(int n, char sor, char dest, char help)
+{
+    if (n == 0)
+    {
+        return;
+    }
+
+    towerOfHanoi(n - 1, sor, help, dest);
+    cout << "Moving to " << sor << " to " << dest << endl;
+    //towerOfHanoi(n - 1, help, dest, sor);
+}
+
+int main()
+{
+
+    towerOfHanoi(3, 'A', 'C', 'B');
+    return 0;
+}
